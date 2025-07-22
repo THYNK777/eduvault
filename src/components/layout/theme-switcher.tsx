3c64e89299daad2695a8fa33d55c91aa5422619d
TEAM thynk <thynk.team2007@gmail.com>
2025-07-22 05:35:42 +0000

    make themes interchanging without any particular order

--- src/components/layout/theme-switcher.tsx
+++ src/components/layout/theme-switcher.tsx
@@ -5,39 +5,23 @@ import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+
+const themes = ['ravenclaw', 'gryffindor', 'slytherin', 'hufflepuff'];
 
 export default function ThemeSwitcher() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    const currentIndex = themes.indexOf(theme || 'ravenclaw');
+    const nextIndex = (currentIndex + 1) % themes.length;
+    setTheme(themes[nextIndex]);
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('ravenclaw')}>
-          Ravenclaw
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('gryffindor')}>
-          Gryffindor
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('slytherin')}>
-          Slytherin
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('hufflepuff')}>
-          Hufflepuff
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 }
