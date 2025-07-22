3c77a0e780f43feaafa4db65a0f35f428a9bd94a
TEAM thynk <thynk.team2007@gmail.com>
2025-07-22 05:36:35 +0000

    no u understood wrong bring back the options

--- src/components/layout/theme-switcher.tsx
+++ src/components/layout/theme-switcher.tsx
@@ -5,23 +5,39 @@ import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
-
-const themes = ['ravenclaw', 'gryffindor', 'slytherin', 'hufflepuff'];
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 export default function ThemeSwitcher() {
-  const { theme, setTheme } = useTheme();
-
-  const toggleTheme = () => {
-    const currentIndex = themes.indexOf(theme || 'ravenclaw');
-    const nextIndex = (currentIndex + 1) % themes.length;
-    setTheme(themes[nextIndex]);
-  };
+  const { setTheme } = useTheme();
 
   return (
-    <Button variant="outline" size="icon" onClick={toggleTheme}>
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('ravenclaw')}>
+          Ravenclaw
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('gryffindor')}>
+          Gryffindor
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('slytherin')}>
+          Slytherin
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('hufflepuff')}>
+          Hufflepuff
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
