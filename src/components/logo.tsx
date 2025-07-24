3f5b8ff917b4edf247e3aacd702d616fe214b10b
TEAM thynk <thynk.team2007@gmail.com>
2025-07-24 15:49:33 +0000

    change the eduvault symbol on the top left to a combination of a vault a

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,11 +10,13 @@ export default function Logo({ className }: { className?: string }) {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className={cn("lucide lucide-square-terminal", className)}
+      className={cn("lucide lucide-vault-scroll", className)}
     >
-      <path d="m7 11 2-2-2-2" />
-      <path d="M11 13h4" />
-      <rect width="18" height="18" x="3" y="3" rx="2" />
+      <rect x="3" y="4" width="18" height="16" rx="2" />
+      <circle cx="12" cy="12" r="4" />
+      <path d="M12 12h0" />
+      <path d="M6 4V2a2 2 0 0 1 2-2h5a2 2 0 0 1 2 2v2" />
+      <path d="M6 20v2a2 2 0 0 0 2 2h5a2 2 0 0 0 2-2v-2" />
     </svg>
   )
 }
